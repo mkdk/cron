a202def1e13a75372ea363776a073e6c9243021e
nnme <makdak69@gmail.com>
2016-07-29 14:12:26 +0300

    fix test var

--- proxy_set.py
+++ proxy_set.py
@@ -36,17 +36,18 @@ def get_proxy():
 def second_filter(proxy):
     best = []
     test = '''<!DOCTYPE html>
-    <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en-US"> <![endif]-->
-    <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en-US"> <![endif]-->
-    <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en-US"> <![endif]-->
-    <!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
-    <head>'''
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+<head>'''
     for i in proxy:
         for i in proxy:
             print('check the proxy %s' % i)
             p = subprocess.Popen('curl -x http://%s -L https://myip.ht' % i, shell=True, stdout=PIPE, stderr=PIPE)
             p.wait()
             out = p.communicate()
+            print out[0]
             if test in out[0]:
                 print("good %s" % i)
                 best.append(i)
