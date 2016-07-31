a7333d7761b629f8d407f4cf70b410e2a84c374c
nnme <makdak69@gmail.com>
2016-07-31 21:26:53 +0300

    dot problem

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ log = os.path.join(HOME, 'out_cron')
 
 def cronic(proxy):
     os.system('echo " " > %s' % log)
-    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}',format(os.path.join(HOME, 'local_viral_scrape'), log))
-    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}',format(os.path.join(HOME, 'local_sans_viral_scrape'), log))
+    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}'.format(os.path.join(HOME, 'local_viral_scrape'), log))
+    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}'.format(os.path.join(HOME, 'local_sans_viral_scrape'), log))
     # os.system(os.path.join(HOME, 'sh/local_sans_viral_scrape.sh'))
     os.system('python %s' % os.path.join(HOME, 'proxychecker.py'))
     change_proxy(os.path.join(HOME, 'local_shazam_us_local'), proxy)
     # os.system(os.path.join(HOME, 'sh/local_shazam_us_local.sh'))
-    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}',format(os.path.join(HOME, 'local_shazam_us_local'), log))
+    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}'.format(os.path.join(HOME, 'local_shazam_us_local'), log))
     # os.system('python %s' % os.path.join(HOME, 'proxychecker.py'))
     print('wait........')
     sleep(5)
     change_proxy(os.path.join(HOME, 'local_shazam_us_localext'), proxy)
     # os.system(os.path.join(HOME, 'sh/local_shazam_us_localext.sh'))
-    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}',format(os.path.join(HOME, 'local_shazam_us_localext'), log))
+    os.system('cd {0} && scrapy crawl music -s LOG_FILE={1}'.format(os.path.join(HOME, 'local_shazam_us_localext'), log))
     sys.exit()
 
 
